dde66256f7eb085ae3dc0780ce1977de63d6cf8c
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-10-03 22:09:52 +0500

    C++ Course

---
+++ Hello_World.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
+// #include<iostream>: Includes the input/output stream library for console 
+// operations.  
+// using namespace std;: Allows the use of standard library features without defining everytime.
+//  int main(): The main function where program execution begins.  
+//  cout<<: Outputs data to the console.  
+//  ; (Semicolon): Terminates a statement in C++.  
+//  Parentheses (): Enclose parameters or conditions (e.g., in main()).  
+//  Curly Braces {}: Define the scope of functions or blocks.  
+//  Delimiters: Separate code elements (e.g., commas, semicolons).  
+//  <<endl: Inserts a new line and flushes the output buffer.  
+//  return 0: Indicates the program ended successfully.
